3db76636cb6954b222f37508ba93ae3ccba74e72
shrikantgond <shrikantgond@gmail.com>
2020-02-04 18:58:09 +0530

    removed setting module from index file

--- app/core/routes/index.route.js
+++ app/core/routes/index.route.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-//router.use('/user', require('../../user/routes/index.route'));
-//router.use('/inventory', require('../../inventory/routes/index.route'));
-router.use('/setting', require('../../setting/routes/index.route'));
+//router.use('/setting', require('../../setting/routes/index.route'));
 
 module.exports = router;
